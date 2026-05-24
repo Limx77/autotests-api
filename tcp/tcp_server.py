8bb8a01d1c32d009ad84fb64da26bdc1cbcaaeb7
Limx77 <ilya0000077@gmail.com>
2026-05-24 12:00:28 +0300

    Create tcp server and client

---
+++ tcp/tcp_server.py
@@ -0,0 +1,26 @@
+import socket
+
+def server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_address = ('localhost', 12345)
+    server_socket.bind(server_address)
+
+
+    server_socket.listen(5)
+    print('Server has been started and waiting for connections...')
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f'Connection from: {client_address}')
+
+        data = client_socket.recv(1024).decode()
+        print(f'Get message: {data}')
+
+        response = f"Server get {data}"
+        client_socket.send(response.encode())
+
+        client_socket.close()
+
+if __name__ == '__main__':
+    server()
