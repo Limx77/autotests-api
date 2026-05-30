a36b9b98452db6836afd856a559c9dd5fba2db28
Limx77 <ilya0000077@gmail.com>
2026-05-30 21:15:35 +0300

    Прохождение темы Практикуемся в использовании API клиентов

--- clients/files/files_client.py
+++ clients/files/files_client.py
@@ -3,7 +3,22 @@ from typing import TypedDict
 from httpx import Response
 
 from clients.api_client import APIClient
+from clients.private_http_builder import get_private_http_client, AuthenticationUserDict
 
+class File(TypedDict):
+    """
+    Описание структуры файла.
+    """
+    id: str
+    filename: str
+    directory: str
+    url: str
+
+class CreateFileResponseDict(TypedDict):
+    """
+    Описание структуры запроса на создание файла.
+    """
+    file: File
 
 class CreateFileRequestDict(TypedDict):
     """
@@ -28,6 +43,11 @@ class FilesClient(APIClient):
         """
         return self.get(f"/api/v1/files/{file_id}")
 
+
+    def create_file(self, request: CreateFileRequestDict)->CreateFileResponseDict:
+        response = self.create_file_api(request)
+        return response.json()
+
     def create_file_api(self, request: CreateFileRequestDict) -> Response:
         """
         Метод создания файла.
@@ -37,7 +57,7 @@ class FilesClient(APIClient):
         """
         return self.post(
             "/api/v1/files",
-            data=request,
+            json=request,
             files={"upload_file": open(request['upload_file'], 'rb')}
         )
 
@@ -49,3 +69,11 @@ class FilesClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.delete(f"/api/v1/files/{file_id}")
+
+def get_files_client(user: AuthenticationUserDict) -> FilesClient:
+    """
+    Функция создаёт экземпляр FilesClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию FilesClient.
+    """
+    return FilesClient(client=get_private_http_client(user))
--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -2,9 +2,21 @@ from clients.api_client import APIClient
 from httpx import Response
 from typing import TypedDict
 
-class CreateUserRequest(TypedDict):
+from clients.public_http_builder import get_public_http_client
+
+class User(TypedDict):
     """
-    Класс для аннотации передаваемого словаря в request create_user_api
+    Описание структуры запроса на создание пользователя.
+    """
+    id: str
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+class CreateUserRequestDict(TypedDict):
+    """
+    Описание структуры запроса на создание пользователя.
     """
     email: str
     password: str
@@ -12,15 +24,35 @@ class CreateUserRequest(TypedDict):
     firstName: str
     middleName: str
 
+class CreateUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: User
+
 class PublicUsersClient(APIClient):
     """
     Клиент для работы с /api/v1/users
     """
 
-    def create_user_api(self, request: CreateUserRequest)-> Response:
+    def create_user_api(self, request: CreateUserRequestDict)-> Response:
         """
         Метод для создания нового юзера api/v1/users
         :param request: словарь CreateUserRequest
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post(url= "api/v1/users",json=request)
+
+
+    def create_user(self,request: CreateUserRequestDict)-> CreateUserResponseDict:
+        response = self.create_user_api(request)
+        return response.json()
+
+
+def get_public_users_client() -> PublicUsersClient:
+    """
+    Функция создаёт экземпляр PublicUsersClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию PublicUsersClient.
+    """
+    return PublicUsersClient(client=get_public_http_client())
